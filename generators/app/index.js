4adda06a29352dcfe80dd68d4909c66b7bcd5bd8
amimaro <amir.zahlan@gmail.com>
2018-06-13 19:49:37 -0300

    :bug: Fix destination

--- generators/app/index.js
+++ generators/app/index.js
@@ -29,33 +29,35 @@ module.exports = class extends Generator {
     return this.prompt(prompts).then(props => {
       this.props = props;
       this.props.slugName = to.slug(this.props.projectName);
+      if (this.appname === this.props.slugName) this.props.destination = '.';
+      else this.props.destination = this.props.slugName;
     });
   }
 
   writing() {
     this.fs.copy(
       this.templatePath('core/.gitignore'),
-      this.destinationPath(`${this.props.slugName}/.gitignore`)
+      this.destinationPath(`${this.props.destination}/.gitignore`)
     );
 
     this.fs.copyTpl(
       this.templatePath('core/docs/.vuepress'),
-      this.destinationPath(`${this.props.slugName}/docs/.vuepress`),
+      this.destinationPath(`${this.props.destination}/docs/.vuepress`),
       this.props
     );
 
     this.fs.copyTpl(
       this.templatePath('core'),
-      this.destinationPath(`${this.props.slugName}/.`),
+      this.destinationPath(`${this.props.destination}/.`),
       this.props
     );
   }
 
   install() {
-    process.chdir(this.props.slugName);
+    process.chdir(this.props.destination);
     this.npmInstall().then(() => {
       this.log('\n\nSuccessfully Done!!');
-      this.log('Run ' + chalk.green('npm run docs:dev') + ' to start.\n');
+      this.log(`Run ${chalk.green(`npm run docs:dev`)} to start.\n`);
     });
   }
 };
