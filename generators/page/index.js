975ce03fd1812bef0b95e20bcef5b73cfacc5eee
Amimaro <amir.zahlan@gmail.com>
2018-06-14 15:59:36 -0300

    :fire: Remove utils

--- generators/page/index.js
+++ generators/page/index.js
@@ -1,6 +1,33 @@
 var Generator = require('yeoman-generator');
 const to = require('to-case');
-const updateConfigs = require('../../utils/updateConfigs');
+const recast = require('recast');
+
+const updateConfigs = function(params) {
+  // Load config file
+  const ast = recast.parse(params.configs);
+  // Filter themeConfig property
+  const themeConfig = ast.program.body[0].expression.right.properties.filter(element => {
+    if (element.key.name === 'themeConfig') return true;
+    return false;
+  })[0];
+  if (!themeConfig) return;
+  // Filter option property
+  const property = themeConfig.value.properties.filter(element => {
+    if (element.key.name === params.option) return true;
+    return false;
+  })[0].value.elements;
+  if (!property) return;
+  const route = JSON.parse(JSON.stringify(property[0]));
+  if (route.elements) {
+    route.elements[0].value = `/${params.props.pageSlug}/`;
+    route.elements[1].value = params.props.pageName;
+  } else {
+    route.properties[0].value.value = params.props.pageName;
+    route.properties[1].value.value = `/${params.props.pageSlug}/`;
+  }
+  property.push(route); // Add new route
+  return recast.print(ast).code;
+};
 
 module.exports = class extends Generator {
   prompting() {
